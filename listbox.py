0bc85dc4c429c809f56fe010078ded8cae65a9dd
bismillah82 <61075885+bismillah82@users.noreply.github.com>
2020-03-14 14:41:59 +0530

    Update listbox.py

--- listbox.py
+++ listbox.py
@@ -1,9 +1,9 @@
 from tkinter import *
 root=Tk()
-
+# displaying the list statement
 def display():
     print(list1.get(ACTIVE))
-
+#giving the entries to the list
 list1=Listbox(root)
 lst_entries=("data1","data2","data3","data4","data5","data6")
 
@@ -12,7 +12,7 @@ for v in lst_entries:
 
 b1=Button(root,text="Get",command=display)
 del_index=int(input("Enter index to delete:"))
-
+#creayting a object and calling the function
 list1.delete(del_index)
 list1.pack()
 b1.pack()
